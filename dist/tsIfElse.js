5aa7d39398679616314fe4c4fab18af3c75ab0a7
Kanjanapas <oshin_2544@hotmail.com>
2023-04-25 14:50:21 +0700

    If Else in TS

---
+++ dist/tsIfElse.js
@@ -0,0 +1,10 @@
+"use strict";
+let amount = 8;
+let result;
+if (amount % 2 === 0) {
+    result = 'Even number';
+}
+else {
+    result = 'Odd number';
+}
+console.log(`Result = ${result}`);
---
+++ src/tsIfElse.ts
@@ -0,0 +1,10 @@
+let amount: number = 8
+let result: string
+
+if (amount % 2 === 0) {
+  result = 'Even number'
+} else {
+  result = 'Odd number'
+}
+
+console.log(`Result = ${result}`)
