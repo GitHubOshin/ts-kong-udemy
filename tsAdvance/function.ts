4c00a6d10f613f4830751f7eb0e775a5a56db260
OSHIN <izkp44@gmail.com>
2023-04-26 15:57:42 +0700

    Update function.ts

--- tsAdvance/function.ts
+++ tsAdvance/function.ts
@@ -1,5 +1,35 @@
 // void
 
-function sayHi() {
+function sayHi(): void {
   console.log('Hi')
 }
+
+sayHi()
+
+// parameter
+
+function myName(name: string) {
+  console.log(`Hi! My name's ${name}`);
+}
+
+myName("Lilo")
+
+// return
+
+function myPet(): string {
+  return `My pet is a cat. Her name is 'Mali'`
+}
+
+console.log(myPet());
+
+// Parameter & Return 
+
+function sumNumber(n: number): string {
+  if (n % 2 === 0) {
+    return 'Even number'
+  } else {
+    return 'Odd number'
+  }
+}
+
+console.log(sumNumber(8));
