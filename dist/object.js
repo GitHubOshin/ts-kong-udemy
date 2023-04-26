79d1883e56ed5a45159c71b354a0b33bc267cd56
Kanjanapas <oshin_2544@hotmail.com>
2023-04-26 19:51:50 +0700

    tsc object.ts

---
+++ dist/object.js
@@ -0,0 +1,26 @@
+"use strict";
+// - How to create an object -
+const personObj1 = { name: 'Me', age: 10 };
+const personObj2 = {
+    name: 'Justin',
+    age: 2,
+    address: "Uk"
+};
+// - Function that gets obj as a property -
+function showDetail1(data) {
+    console.log(`Name: ${data.name}, Age: ${data.age}`);
+}
+showDetail1(personObj2); // OUTPUT -> Name: Juatin, Age: 2
+// - Excess Properties Check -
+function showDetail2(data) {
+    console.log(`Name: ${data.name}, Age: ${data.age}`);
+}
+showDetail2({ personObj2 }); // Put obj inside {}
+// - Func returns obj -
+function randomPosition() {
+    return {
+        lat: Math.random(),
+        long: Math.random()
+    };
+}
+console.log(randomPosition());
