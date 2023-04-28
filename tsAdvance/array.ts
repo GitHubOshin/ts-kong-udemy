d7b457f49dfb1524c7ac7b528af4abf1e4ed5634
OSHIN <izkp44@gmail.com>
2023-04-28 23:50:51 +0700

    Array & TypeAliases

--- tsAdvance/array.ts
+++ tsAdvance/array.ts
@@ -1,5 +1,17 @@
 const myFriends: string[] = []
-const myPets:string[] = ["Dog", "Cat"]
+const myPets: string[] = ['Dog', 'Cat']
 
-console.log(myPets[0]);
-console.log(myPets.length);
+console.log(myPets[0])
+console.log(myPets.length)
+
+// Array & TypeAliases
+
+type BestFriend = {
+  name: string
+  readonly birthday: string
+  contact: string
+  favouriteFood?: string
+}
+
+const bestFriends: BestFriend[] = []
+bestFriends.push({ name: 'Sayo', birthday: '01/01/2001', contact: 'Instagram' })
