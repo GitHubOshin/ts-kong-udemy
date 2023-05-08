90be3e7c00d98321dbaab544ff1a814a42a734e3
OSHIN <izkp44@gmail.com>
2023-05-09 04:05:24 +0700

    Function Overloading

--- tsAdvance/function.ts
+++ tsAdvance/function.ts
@@ -82,3 +82,40 @@ const avatarFriends = {
 
 const { cat, hamster, dog } = avatarFriends
 console.log(cat, hamster, dog)
+
+// Function Overloading
+
+// -- 1 --
+function sayHiToFriend(): string
+function sayHiToFriend(name: string): string
+function sayHiToFriend(name?: unknown): unknown {
+  if (!name) {
+    return 'Hello Friend!'
+  }
+  if (typeof name === 'string') {
+    return `Hello ${name}`
+  }
+  throw new Error('TypeError')
+}
+
+console.log(sayHiToFriend())
+console.log(sayHiToFriend('Oshin'))
+
+// -- 2 --
+
+function sumApple(newApple: number, oldApple: number): number
+function sumApple(newApple: string, oldApple: string): string
+function sumApple(newApple: unknown, oldApple: unknown): unknown {
+  if (typeof newApple === 'number' && typeof oldApple === 'number') {
+    return newApple + oldApple
+  }
+  if (typeof newApple === 'string' && typeof oldApple === 'string') {
+    return `New apple(s): ${newApple} + Old apple(s): ${oldApple} | All = ${
+      parseInt(newApple) + parseInt(oldApple)
+    }`
+  }
+  throw new Error('TypeError')
+}
+
+console.log(sumApple(2, 2))
+console.log(sumApple('6', '1'))
