50cb2b337458fadc6655d9688b27503fc8990711
Kanjanapas <oshin_2544@hotmail.com>
2023-05-09 05:14:07 +0700

    tsc tuple.ts

---
+++ dist/tsAdvance/tuple.js
@@ -0,0 +1,11 @@
+"use strict";
+// Tuple
+const point = [10, 'Pink Card'];
+console.log(point);
+console.log(point[1]);
+console.log((point[0] = 20));
+console.log(point);
+// Tuple Lable
+const myFriend = ['Ngaw', 14];
+const ngaw = ['Vietnam', 14];
+console.log(ngaw);
