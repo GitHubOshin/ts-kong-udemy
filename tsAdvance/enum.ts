ba5ea535aa8733d5e426ad9216cda647f1124f3e
OSHIN <izkp44@gmail.com>
2023-05-09 06:01:59 +0700

    Create enum.ts

---
+++ tsAdvance/enum.ts
@@ -0,0 +1,15 @@
+enum LoveLevel {
+  Never,
+  DontTouchMyFood,
+  IcanShareMyFood,
+  YourFoodIsMine
+}
+
+const myLoveForRare: LoveLevel = LoveLevel.YourFoodIsMine
+
+enum Hungry {
+  No = 0,
+  CanEatButNotHungry = 1,
+  ImHungry = 50,
+  CantWaitAnymore = 100
+}
