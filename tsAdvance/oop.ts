79e9cc1407fdfdb4c056f4361bb549ba7e2ebdaf
OSHIN <izkp44@gmail.com>
2023-05-12 23:39:48 +0700

    Constructor

--- tsAdvance/oop.ts
+++ tsAdvance/oop.ts
@@ -5,3 +5,13 @@ class Employee {}
 
 const epm1 = new Employee()
 console.log(epm1)
+
+// -- Constructor --
+
+class Employee2 {
+  constructor() {
+    console.log('Hello, Employee2')
+  }
+}
+
+const emp2 = new Employee2()
