4ca7a54503f29b91fb28c2e23fb62ea55470530d
OSHIN <izkp44@gmail.com>
2023-05-13 06:13:26 +0700

    Access Modifiers

--- tsAdvance/oop.ts
+++ tsAdvance/oop.ts
@@ -30,3 +30,20 @@ class Employee3 {
 }
 
 const emp3 = new Employee3('Kong', 'IT', 30)
+
+// -- Access Modifiers --
+
+class Employee4 {
+  private name: string
+  private department: string
+  private salary: number
+  constructor(name: string, department: string, salary: number) {
+    this.name = name
+    this.department = department
+    this.salary = salary
+  }
+}
+
+const emp4 = new Employee4('Nung Jin', 'IT', 10)
+console.log(emp4)
+// emp4.salary = 20 // Property 'salary' is private and only accessible within class 'Employee4'.
