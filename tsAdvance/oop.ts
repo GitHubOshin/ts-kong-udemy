ed40367aef0386c7eead1ea2cc1bf3de9322ff3e
OSHIN <izkp44@gmail.com>
2023-05-13 07:03:29 +0700

    Getter Method & Setter Method

--- tsAdvance/oop.ts
+++ tsAdvance/oop.ts
@@ -49,7 +49,6 @@ console.log(emp4)
 // emp4.salary = 20 // Property 'salary' is private and only accessible within class 'Employee4'.
 
 // --  Method --
-
 class Employee5 {
   name: string
   department: string
@@ -68,3 +67,37 @@ class Employee5 {
 
 const emp5 = new Employee5('Meow', 'IT', 2)
 emp5.showDetail()
+
+// -- Getter Method & Setter Method --
+
+class Employee6 {
+  name: string
+  department: string
+  salary: number
+  constructor(name: string, department: string, salary: number) {
+    this.name = name
+    this.department = department
+    this.salary = salary
+  }
+  showDetail() {
+    console.log(
+      `Name: ${this.name} | Department: ${this.department} | Salary: ${this.salary} THB`
+    )
+  }
+  // setSalary(newSalary: number) {
+  set Salary(newSalary: number) {
+    this.salary = newSalary
+  }
+  // getSalary(): number {
+  get Salary(): number {
+    return this.salary
+  }
+}
+
+const emp6 = new Employee6('Lala', 'IT', 20)
+// console.log(emp6.getSalary())
+console.log('Salary: ' + emp6.Salary)
+// emp6.setSalary(5)
+emp6.Salary = 5
+// console.log(emp6.getSalary())
+console.log('Salary: ' + emp6.Salary)
