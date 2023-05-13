400c0e0dee5c5418a0e9e1e195d1af01e06b1067
OSHIN <izkp44@gmail.com>
2023-05-13 07:38:58 +0700

    Create 6-parameter-properties.ts

---
+++ tsAdvance/oop/6-parameter-properties.ts
@@ -0,0 +1,27 @@
+'use strict'
+// -- Parameter Properties --
+
+class Employee7 {
+  constructor(
+    private name: string,
+    private department: string,
+    private salary: number
+  ) {}
+  showDetail() {
+    console.log(
+      `Name: ${this.name} | Department: ${this.department} | Salary: ${this.salary} THB`
+    )
+  }
+  set Salary(newSalary: number) {
+    this.salary = newSalary
+  }
+  get Salary(): number {
+    return this.salary
+  }
+}
+
+const emp7 = new Employee7('Cat Tung', 'Eng Teacher', 20)
+console.log(emp7.Salary)
+emp7.Salary = 100
+emp7.showDetail()
+console.log(emp7.Salary)
