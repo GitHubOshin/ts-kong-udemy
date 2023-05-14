339c68e43691737d5590a9f48b9da02e09056e78
OSHIN <izkp44@gmail.com>
2023-05-14 23:33:43 +0700

    Create 8-super.ts

---
+++ tsAdvance/oop/8-super.ts
@@ -0,0 +1,27 @@
+class Employee9 {
+  constructor(
+    private name: string,
+    private department: string,
+    private salary: number
+  ) {}
+  showDetail() {
+    console.log(
+      `Name: ${this.name} | Department: ${this.department} | Salary: ${this.salary} THB`
+    )
+  }
+  set Salary(newSalary: number) {
+    this.salary = newSalary
+  }
+  get Salary(): number {
+    return this.salary
+  }
+}
+
+class IT9 extends Employee9 {
+  constructor(name: string, salary: number) {
+    super(name, 'IT', salary)
+  }
+}
+
+const emp8 = new IT9('Lisa', 9)
+emp8.showDetail()
