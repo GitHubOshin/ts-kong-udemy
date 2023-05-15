005ee0a198cb8639f80a158e50e32da96c7e6a75
OSHIN <izkp44@gmail.com>
2023-05-15 23:13:40 +0700

    Update 1-generic.ts: getElementString

--- tsAdvance/generic/1-generic.ts
+++ tsAdvance/generic/1-generic.ts
@@ -5,6 +5,13 @@ function getElementNumber(arr: number[], count: number) {
   return arr.slice(arr.length - count)
 }
 
+// String
+function getElementString(arr: string[], count: number) {
+  return arr.slice(arr.length - count)
+}
+
 const arrNum: number[] = [10, 20, 30, 40]
+const arrStr: string[] = ['Tung Cat', 'Kman', 'Oshin', 'Icey']
 
 console.log(getElementNumber(arrNum, 2))
+console.log(getElementString(arrStr, 2))
