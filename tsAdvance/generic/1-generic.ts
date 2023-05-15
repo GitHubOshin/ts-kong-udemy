0e47748156e398336554f8fbc06e410dee3289fb
OSHIN <izkp44@gmail.com>
2023-05-15 23:14:24 +0700

    Update 1-generic.ts: getElementBoolean

--- tsAdvance/generic/1-generic.ts
+++ tsAdvance/generic/1-generic.ts
@@ -10,8 +10,15 @@ function getElementString(arr: string[], count: number) {
   return arr.slice(arr.length - count)
 }
 
+// Boolean
+function getElementBoolean(arr: boolean[], count: number) {
+  return arr.slice(arr.length - count)
+}
+
 const arrNum: number[] = [10, 20, 30, 40]
 const arrStr: string[] = ['Tung Cat', 'Kman', 'Oshin', 'Icey']
+const arrBoo: boolean[] = [true, false, true, false]
 
 console.log(getElementNumber(arrNum, 2))
 console.log(getElementString(arrStr, 2))
+console.log(getElementBoolean(arrBoo, 2))
