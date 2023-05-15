bedb6bbd1109ac9c3dc6c255531428857cf5f378
OSHIN <izkp44@gmail.com>
2023-05-15 23:12:23 +0700

    Update 1-generic.ts: getElementNumber

--- tsAdvance/generic/1-generic.ts
+++ tsAdvance/generic/1-generic.ts
@@ -1,1 +1,10 @@
 // -- Generic --
+
+// Number
+function getElementNumber(arr: number[], count: number) {
+  return arr.slice(arr.length - count)
+}
+
+const arrNum: number[] = [10, 20, 30, 40]
+
+console.log(getElementNumber(arrNum, 2))
