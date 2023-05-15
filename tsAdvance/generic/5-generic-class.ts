0ae55caf9dedee25cea6079beaabc60e8a5f1be6
OSHIN <izkp44@gmail.com>
2023-05-16 03:56:58 +0700

    Update 5-generic-class.ts: Generic Class

--- tsAdvance/generic/5-generic-class.ts
+++ tsAdvance/generic/5-generic-class.ts
@@ -8,9 +8,23 @@ class Stack1 {
   }
 }
 
+// Generic Class
+class Stack2<T> {
+  private items: T[] = []
+  pushElement(element: T) {
+    this.items.push(element)
+  }
+}
+
 const stack1 = new Stack1()
+const stack2 = new Stack2<number>()
 
 stack1.pushElement('Cola')
 stack1.pushElement('Pepsi')
 
+stack2.pushElement(200)
+stack2.pushElement(550)
+
 console.log(stack1)
+
+console.log(stack2)
